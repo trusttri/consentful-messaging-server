828aff9325fef805808cdc792676b1b49e2a026c
imjane <imjane@umich.edu>
2020-10-11 22:33:12 -0400

    set up django

---
+++ consentful_messaging/consentful_messaging/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for consentful_messaging project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consentful_messaging.settings')
+
+application = get_wsgi_application()
