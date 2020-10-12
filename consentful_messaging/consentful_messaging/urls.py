931b957f3c79659c0f8f7fda1f6f85b60d6ee089
trusttri <alwaysjane15@gmail.com>
2020-10-11 23:37:43 -0400

    initialized index.html for landing page

--- consentful_messaging/consentful_messaging/urls.py
+++ consentful_messaging/consentful_messaging/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from website.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('admin/', admin.site.urls),
+    url(r'^$', index),
 ]
--- consentful_messaging/website/views.py
+++ consentful_messaging/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+def index(request):
+	template = loader.get_template('website/index.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
