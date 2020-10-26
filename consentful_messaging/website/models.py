7c6634c7f0983745c4fe89b3a28c6dd5717db231
jklaris <jklaris@umich.edu>
2020-10-26 16:55:26 -0400

    Added TwitterAccount model in models.py

--- consentful_messaging/website/models.py
+++ consentful_messaging/website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TwitterAccount(models.Model):
+    id = models.AutoField(primary_key=true)
+    screen_name = models.TextField()
