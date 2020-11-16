6cc22f8dbbd92cce518167d4488ef5d894d46fe0
anniechn <anniechn@umich.edu>
2020-11-16 16:17:20 -0600

    twitter auth function

--- consentful_messaging/website/views.py
+++ consentful_messaging/website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, JsonResponse
-# Create your views here.
+import tweepy
+
 def index(request):
 	template = loader.get_template('website/index.html')
 	context = {}
@@ -11,4 +12,11 @@ def author_network_rules(request):
     user = request.GET.get('user')
     sender = request.GET.get('sender')
     return HttpResponse("false")
+    
+def twitter_auth(consumerkey, consumersecret, token, tokensecret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    return auth
+    
+    
 	
