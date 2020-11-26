df519fe1292a69b04b5bfa2e8415c09d7f4aedfd
trusttri <alwaysjane15@gmail.com>
2020-11-26 10:51:43 +0900

    fixed twitter authentication related code

--- consentful_messaging/website/views.py
+++ consentful_messaging/website/views.py
@@ -12,6 +12,9 @@ def index(request):
 def author_network_rules(request):
     user = request.GET.get('user')
     sender = request.GET.get('sender')
+
+    api = twitter_api_auth_using_csv()
+
     return HttpResponse("false")
     
 # helper function for authenticating API keys
@@ -23,16 +26,16 @@ def twitter_api_auth(consumer_key, consumer_secret, acc_key, acc_secret):
     
 # API key authentication
 def twitter_api_auth_using_csv():
-   with open('twitter-creds.csv') as csv_file
-      reader = csv.reader(csv_file, delimiter=',')
-      for row in reader:
-         consumer_key = row['consumer_key']
-         consumer_secret = row['consumer_secret']
-         acc_key = row['access_key']
-         acc_secret = row['access_secret']
-         twitter_api_auth(consumer_key, consumer_secret, acc_key, acc_secret)
-         
-         
+	import os
+	workpath = os.path.dirname(os.path.abspath(__file__))
+	with open(os.path.join(workpath, 'twitter-creds.csv'), 'rt') as csv_file:
+		reader = csv.DictReader(csv_file, delimiter=',')
+		for row in reader:
+			consumer_key = row['consumer_key']
+			consumer_secret = row['consumer_secret']
+			acc_key = row['access_key']
+			acc_secret = row['access_secret']
+		return twitter_api_auth(consumer_key, consumer_secret, acc_key, acc_secret)
          
       
    
