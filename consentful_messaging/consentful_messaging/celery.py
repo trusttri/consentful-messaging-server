15295531d4fca740bca8568ed7f439b739a8d345
trusttri <alwaysjane15@gmail.com>
2021-01-09 02:25:58 +0900

    fixes to make celery command work

--- consentful_messaging/consentful_messaging/celery.py
+++ consentful_messaging/consentful_messaging/celery.py
@@ -2,13 +2,16 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consentful_messaging.settings')
+
+import django
+django.setup()
 
 from django.conf import settings
 from celery import Celery
 
-app = Celery('website')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('consentful_messaging')
+app.config_from_object('django.conf:settings', namespace="CELERY")
 
 # For autodiscover_tasks to work, you must define your tasks in a file called 'tasks.py'.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- consentful_messaging/website/tasks.py
+++ consentful_messaging/website/tasks.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
-from celery import shared_task, current_task
+from celery import Celery, shared_task, current_task
 from celery.exceptions import Ignore
 from website.models import TwitterAccount
 from django.utils import timezone
 import tweepy
+
+celery = Celery('tasks', broker='amqp://guest@localhost//') #!
+
 
 @shared_task
 def network_rules(user, sender, access_key, access_secret):
