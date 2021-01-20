5ad4d1dd2ef1a5002e54174907f88018947486d4
trusttri <alwaysjane15@gmail.com>
2021-01-20 15:55:03 +0900

    removed wrong configuration

--- consentful_messaging/website/tasks.py
+++ consentful_messaging/website/tasks.py
@@ -5,8 +5,6 @@ from website.models import TwitterAccount
 from django.utils import timezone
 import tweepy
 
-celery = Celery('tasks', broker='amqp://guest@localhost//') #!
-
 
 @shared_task
 def network_rules(user, sender, access_key, access_secret):
