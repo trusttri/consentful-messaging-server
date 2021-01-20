2785d0d2aac63c413b45e1826213c9c678787a05
trusttri <alwaysjane15@gmail.com>
2021-01-20 16:03:18 +0900

    updated network rules

---
+++ consentful_messaging/website/network_rules.py
@@ -0,0 +1,22 @@
+def check_follower_num(user, sender):
+	return False
+
+def check_follow(user, sender):
+	return False
+
+def check_mutuals(user, sender):
+	return False
+
+def check_tweet_history(user, sender):
+	return False
+
+def check_message_history(user, sender):
+	return False
+
+def check_like_history(user, sender):
+	return False
+
+def check_mutual_block(user, sender):
+	return False
+
+
