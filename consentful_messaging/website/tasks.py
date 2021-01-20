74db1b960fdddfdecb20b14707a65b86f4005fb8
trusttri <alwaysjane15@gmail.com>
2021-01-20 20:12:51 +0900

    cache twitter account

--- consentful_messaging/website/tasks.py
+++ consentful_messaging/website/tasks.py
@@ -8,20 +8,26 @@ from website.authentication import twitter_api_auth, twitter_api_auth_using_csv
 from website.network_functions import check_follower_num
 import csv, json
     
+
 def get_user_information(username):
-	api = twitter_api_auth_using_csv()
-	user = api.get_user(username)
-	if(user.protected):
-		print("User is Private")
-		return
-	else:
-		userId = user.id_str
-		userScreenName = user.screen_name
-		userDateCreated = user.created_at
-		userNumFollowers = user.followers_count
-		newAccount = TwitterAccount(screen_name=userScreenName,created_date=userDateCreated,follower_num=userNumFollowers) 
-		newAccount.save()
+	newAccount = TwitterAccount.objects.filter(screen_name=username)
+	if len(newAccount) > 0:
 		return newAccount
+	else:
+		api = twitter_api_auth_using_csv()
+		user = api.get_user(username)
+		if(user.protected):
+			print("User is Private")
+			return
+		else:
+			userId = user.id_str
+			userScreenName = user.screen_name
+			userDateCreated = user.created_at
+			userNumFollowers = user.followers_count
+			newAccount = TwitterAccount(screen_name=userScreenName,created_date=userDateCreated,follower_num=userNumFollowers) 
+			newAccount.save()
+			return newAccount
+
 
 @shared_task
 def network_rules(user_name, sender_name):
