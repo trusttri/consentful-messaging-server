044e0749271c9986a36e93186fe0e74fa60f1a57
anniechn <anniechn@umich.edu>
2021-02-04 14:34:50 -0600

    everything should be updated

--- consentful_messaging/website/views.py
+++ consentful_messaging/website/views.py
@@ -17,7 +17,6 @@ def index(request):
 
 @csrf_exempt
 def author_network_rules(request):
-<<<<<<< HEAD
 	user_name = request.GET.get('user')
 	sender_name = request.GET.get('sender')
 
@@ -55,57 +54,6 @@ def poll_status(request):
 	elif task.state == "PENDING" or task.state == "RECEIVED" or task.state == "STARTED":
 		data['state'] = "PENDING"
 		data['result'] = "PENDING"
-=======
-  user = request.GET.get('user')
-  sender = request.GET.get('sender')
-  print(user, sender)
-
-  # return placeholder response for now
-  data = {'response': 'True'}
-  json_data = json.dumps(data)
-  response = HttpResponse(json_data, content_type='application/json')
-  response["Access-Control-Allow-Origin"] = "*"
-  response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE, HEAD"
-  response["Access-Control-Max-Age"] = "1000"
-  response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
-  print(response)
-  return response
-
-# helper function for API key authentication
-def twitter_api_auth(consumer_key, consumer_secret, acc_key, acc_secret):
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(acc_key, acc_secret)
-  api = tweepy.API(auth, wait_on_rate_limit=True)
-  try:
-  	api.verify_credentials()
-  	print("Authentication OK")
-  except:
-  	print("Error during authentication")
-  return api
-    
-# API key authentication
-def twitter_api_auth_using_csv():
-	import os
-	workpath = os.path.dirname(os.path.abspath(__file__))
-	with open(os.path.join(workpath, 'twitter-creds.csv'), 'rt') as csv_file:
-		reader = csv.DictReader(csv_file, delimiter=',')
-		for row in reader:
-			consumer_key = row['consumer_key']
-			consumer_secret = row['consumer_secret']
-			acc_key = row['access_key']
-			acc_secret = row['access_secret']
-		try:
-			return twitter_api_auth(consumer_key, consumer_secret, acc_key, acc_secret)
-		except NameError:
-			raise RuntimeError("Check if you have Twitter API keys in the csv file.")
-
-def get_user_information(username):
-	api = twitter_api_auth_using_csv()
-	user = api.get_user(username)
-	if(user.protected):
-		print("User is Private")
-		return
->>>>>>> b80884d... separate file temp
 	else:
 		data['state'] = "FAILURE"
 		data['result'] = "FAILURE"
@@ -120,24 +68,4 @@ def get_user_information(username):
 	print(response)
 	return response
 
-
-
-
-def get_user_200_tweets(username):
-	api = twitter_api_auth_using_csv()
-	try:
-		statuses = api.user_timeline(screen_name = username,count=200, tweet_mode="extended")
-	except tweepy.TweepError as e:
-		return 'Not authorized.'
-			
-	tweet_info = {}
-
-	for tweet in statuses:
-		if hasattr(tweet, 'retweeted_status'):
-			tweet_info[tweet.id] = {'text': tweet.retweeted_status.full_text, 'tweet_time': tweet.created_at,
-			'urls': extract_urls(tweet.retweeted_status)}
-		else:
-			tweet_info[tweet.id] = {'text': tweet.full_text, 'tweet_time': tweet.created_at,
-			'urls': extract_urls(tweet)}
-	return tweet_info
 
