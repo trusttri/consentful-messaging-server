57c66f8b7e5e045b9f18c08c5b23a7b6f2e54217
jklaris <jklaris@umich.edu>
2021-02-16 09:24:41 -1000

    added fields to TwitterAccount added functions to store followers and following for the user :wq

--- consentful_messaging/website/models.py
+++ consentful_messaging/website/models.py
@@ -6,7 +6,15 @@ from django.db.models import IntegerField, Model
 
 class TwitterAccount(models.Model):
   id = models.AutoField(primary_key=True)
-  screen_name = models.TextField()
-  created_date = models.DateField(auto_now=False,auto_now_add=False)
-  follower_num = models.IntegerField()
-  followers = models.ManyToManyField("self", symmetrical=False, related_name="following")
+  screen_name = models.TextField(blank=True, null=True)
+  created_date = models.DateField(auto_now=False,auto_now_add=False,blank=True, null=True)
+  follower_num = models.IntegerField(blank=True, null=True)
+  following = models.ManyToManyField("self", symmetrical=False, related_name="following",blank=True, null=True) #list of users that current user follows
+  followers = models.ManyToManyField("self", symmetrical=False, related_name="followers",blank=True, null=True) #list of users that follow currentUser
+  twitter_id = models.TextField() #id of user
+  suspended = models.BooleanField(default=False,blank=True, null=True) #suspended?
+  protected = models.BooleanField(blank=True, null=True) #protected?
+  following_num = models.IntegerField(blank=True, null=True) # add code in tasks to retrieve following_num from api
+
+
+  #code for storing list of following and followers using network_functions cursor method
--- consentful_messaging/website/tasks.py
+++ consentful_messaging/website/tasks.py
@@ -24,9 +24,31 @@ def get_user_information(username):
 			userScreenName = user.screen_name
 			userDateCreated = user.created_at
 			userNumFollowers = user.followers_count
-			newAccount = TwitterAccount(screen_name=userScreenName,created_date=userDateCreated,follower_num=userNumFollowers) 
+			userProtected = user.protected
+			userFollowingNum = user.friends_count
+			newAccount = TwitterAccount(screen_name=userScreenName,created_date=userDateCreated,follower_num=userNumFollowers,twitter_id=userId,protected=userProtected, following_num=userFollowingNum) 
+			#need to get suspended info 
+
+			getFollowers(api,newAccount)
+			getFollowing(api,newAccount)
+
 			newAccount.save()
 			return newAccount
+
+def getFollowers(api, twitter_account):
+	#getting all followers: followers.ids() seems to return all id's, not sure if there is a max
+	follower_list = api.followers_ids(twitter_account.screen_name)
+	for follower in follower_list:
+		followerAccount = TwitterAccount(twitter_id=follower)
+		twitter_account.followers.add(followerAccount)
+
+def getFollowing(api, twitter_account):
+	#getting all following -- friends.ids() returns a lot of id's, not sure what the max is
+	following_list = api.friends_ids(twitter_account.screen_name)
+	for follow in following_list:
+		followAccount = TwitterAccount(twitter_id=follow)
+		twitter_account.following.add(followAccount)
+
 
 
 @shared_task
